7f84d3f32435cc5cf8cefb3cd6db640a8bdf35e0
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-06 19:52:51 -0300

    test: app base config

---
+++ test/common/config/app.config.spec.ts
@@ -0,0 +1,40 @@
+import { appConfig } from '@/common/config/app.config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { Test, TestingModuleBuilder } from '@nestjs/testing'
+
+describe('appConfig', () => {
+  jest.clearAllMocks()
+  let configService: ConfigService
+  let testingModuleBuilder: TestingModuleBuilder
+  beforeEach(() => {
+    testingModuleBuilder = Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: 'test.env',
+          expandVariables: true,
+          cache: false
+        }),
+        ConfigModule.forFeature(appConfig())
+      ],
+      controllers: [],
+      providers: []
+    })
+  })
+  it('should have appropriate app configs', async () => {
+    const moduleRef = await testingModuleBuilder.compile()
+    configService = moduleRef.get<ConfigService>(ConfigService)
+    const configs = configService.get('app')
+    expect(configs).toHaveProperty('name')
+    expect(configs).toHaveProperty('description')
+    expect(configs).toHaveProperty('version')
+    expect(configs).toHaveProperty('port')
+    expect(configs).toHaveProperty('baseUrl')
+    expect(configs).toHaveProperty('baseUri')
+    expect(configs).toHaveProperty('docsUri')
+  })
+  it('should throw a error if appropriate app configs is not present', async () => {
+    const moduleRef = testingModuleBuilder.compile()
+    delete process.env.APP_NAME
+    await expect(moduleRef).rejects.toThrow()
+  })
+})
