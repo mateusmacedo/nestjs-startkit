44f6ff9d6dc9d167ed81af1d21dba15d3e3458f4
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-06 19:54:19 -0300

    feat: cors, versioning and swagger settings

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { appConfig } from '@/common/config/app.config'
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      envFilePath: '.env',
+      expandVariables: process.env.NODE_ENV !== 'production',
+      cache: true
+    }),
+    ConfigModule.forFeature(appConfig())
+  ]
+})
 export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
 import './module.alias'
-import { NestFactory } from '@nestjs/core'
+import { NestFactory, Reflector } from '@nestjs/core'
 import { MainModule } from '@/main.module'
+import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule)
-  await app.listen(3000)
+  const app = await NestFactory.create(MainModule, { bufferLogs: true })
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*'
+  })
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validationError: { target: true, value: true },
+      whitelist: true
+    })
+  )
+  app.enableVersioning({
+    type: VersioningType.URI
+  })
+  const configService = app.get(ConfigService)
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>('app.name'))
+    .setDescription(configService.get<string>('app.description'))
+    .setVersion(configService.get<string>('app.version'))
+    .build()
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => `${controllerKey}:${methodKey}`
+  }
+  const document = SwaggerModule.createDocument(app, config, options)
+  SwaggerModule.setup(configService.get<string>('app.docsUri'), app, document)
+  await app.listen(configService.get<number>('app.port') ?? 3000)
 }
 bootstrap()
+  .then(() => true)
+  .catch((err) => console.error(err))
